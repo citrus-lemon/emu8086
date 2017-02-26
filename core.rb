4baf8b69297ea4f9f36829c75bcf875ed1393db2
WindProphet <lihanyuan1996@gmail.com>
2017-02-26 15:01:29 +0800

    DataEle

--- core.rb
+++ core.rb
@@ -1,3 +1,4 @@
+require "./InstructionSet"
 class CPU
   def initialize(copy=nil)
     unless copy
@@ -21,14 +22,37 @@ class CPU
         ["AX","CX","DX","BX","SP","BP","SI","DI"]
       ]
 
+      @@reg_seg = ["ES","CS","SS","DS"]
+      @@d_tag = ["from","to"]
+      @@w_tag = ["byte","word"]
+      @@rm_tab = [
+        ["BX","SI"],
+        ["BX","DI"],
+        ["BP","SI"],
+        ["BP","DI"],
+        ["SI"],
+        ["DI"],
+        ["BP"],
+        ["BX"],
+      ]
+
       class << self
         attr_accessor :self
         def reg_tab() @@reg_tab; end
-        def reg(d,w)
+
+        def reg(d,w=2)
           e = self.new
           e[:class] = "reg"
-          e[:sign] = e[:name] = @@reg_tab[w][d]
-          e[:word] = w
+          if d.class == String
+            e[:sign] = e[:name] = d; e[:word] = 0; return e if ["AL","CL","DL","BL","AH","CH","DH","BH"].include?(d)
+            e[:sign] = e[:name] = d; e[:word] = 1; return e if ["AX","CX","DX","BX","SP","BP","SI","DI","ES","CS","SS","DS","PC"].include?(d)
+            throw "invaild register name"
+          else
+            e[:sign] = e[:name] = @@reg_tab[w][d]
+            e[:word] = w
+            throw "need Word Instruction param" if w == 2
+          end
+          e
         end
         def r_mem(mod,rm,w)
           case mod
@@ -42,22 +66,85 @@ class CPU
               disp = @@self.fetchw
               e[:addr] = @@self.DS * 16 + disp
               e[:sign] = "[0%04xH]" % disp
+            else
+              e[:addr] = @@self.DS * 16
+              e[:sign] = @@rm_tab[rm].map { |el|
+                r = self.reg(el)
+                e[:addr] += r.data
+                r.name
+              }.join('+')
+              disp = case mod
+              when 0 then 0
+              when 1 then @@self.fetchb
+              when 2 then @@self.fetchw
+              end
+              e[:addr] += disp
+              e[:sign] += (mod == 0) ? "" : ("+%0#{mod*2}xH" % disp)
+              e[:sign] = "[#{e[:sign]}]"
             end
           end
+          e
+        end
+        def mem(addr,w=0,static = 0)
+          e = self.new
+          e[:class] = "mem"
+          e[:addr] = @@self.DS * 16 + addr
+          e[:word] = w
+          e[:sign] = "[%0#{(w+1)*2}xH]" % addr
+          e
+        end
+        def imm(v,w=0)
+          e = self.new
+          e[:class] = "imm"
+          w = 1 if v >= 0x100
+          e[:value] = v % 0x10000
+          STDERR.puts "Immediate data overflow" if v >= 0x10000
+          e[:word] = w
+          e
         end
       end
 
+      def name; self[:name]; end
+      def w   ; self[:word]; end
+
       def data=(d)
-
+        data = unless d.class == self.class
+          d % 0x10000
+        else
+          throw "length difference between object and source" if (d[:class] == "mem" && d.w != self.w) || (d.w > self.w)
+          d.data
+        end
+        case self[:class]
+        when "reg"
+          @@self.method(self[:name]+"=").call(data)
+        when "mem"
+          if self.w == 0
+            @@self.memory[self[:addr]] = data % 0x100
+          else
+            @@self.memory[self[:addr]] = data % 0x100
+            @@self.memory[self[:addr]+1] = (data >> 8) % 0x100
+          end
+        when "imm"
+          throw "Immediate data cannot be assigned"
+        end
       end
-      def data
 
+      def data
+        case self[:class]
+        when "reg" then @@self.method(self[:name]).call
+        when "imm" then self[:value]
+        when "mem" then @@self.getmem(self[:addr]) + (self[:word] == 1 ? (@@self.getmem(self[:addr] + 1) << 8) : 0)
+        end
+      end
+
+      def addr
+        self[:addr] if self[:class] == "mem"
       end
     end
 
   end
   attr_accessor :memory, :FLAG, :AX, :BX, :CX, :DX, :SI, :DI, :BP, :SP, :PC, :CS, :DS, :ES, :SS
-  attr_reader :disass, :pos, :codeparse
+  attr_reader :disass, :pos, :codeparse, :DataEle
 
   # Flags
   [
@@ -94,6 +181,8 @@ class CPU
     end
   end
 
+  include InstructionSet
+
   def load_code(code)
     if code.class == Array
       @codeline = code.length
@@ -108,14 +197,17 @@ class CPU
     b
   end
 
+  def getmem(addr)
+    a = @memory[addr]
+    a = a ? a % 0x100 : 0
+  end
+
   def fetchw
     fetchb + (fetchb << 8)
   end
 
-
-
-  def test
-    @DataEle.r_mem(0,6,1)
+  def test(*op)
+    @DataEle.r_mem(*op)
   end
 
 end
--- test.rb
+++ test.rb
@@ -1,8 +1,13 @@
 require "./core"
 
-code = File.open("codegolf.8086")
-@cpu = CPU.new
+# code = File.open("codegolf.8086")
+@a = CPU.new
+@a.CS = 2
+@a.load_code([0x12,0x34,0x56])
+@a.BP = 4
+@a.DI = 8
 
-@cpu.load_code([
-  0b10111
-  ])
+@a.test 0,3,0
+
+require 'irb'
+IRB.start
