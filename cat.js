59be36fafa588f239cabb210b4afc3d8914b8dc0
WindProphet <lihanyuan1996@gmail.com>
2017-04-01 22:26:51 +0800

    rename node-module-concat

--- cat.js
+++ cat.js
@@ -1,5 +1,5 @@
 // concatenate the library files to web-client
-const modConcat = require("node-module-concat");
+const modConcat = require("module-concat");
 const outputFile = "./src/concatenated.js";
 modConcat("./index.js", outputFile, function(err, stats) {
     if(err) throw err;
