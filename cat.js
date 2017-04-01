3aae98ae98c3bdd73c6812c5b916cc0bef741c36
WindProphet <lihanyuan1996@gmail.com>
2017-04-01 22:19:17 +0800

    concatenated

---
+++ cat.js
@@ -0,0 +1,7 @@
+// concatenate the library files to web-client
+const modConcat = require("node-module-concat");
+const outputFile = "./src/concatenated.js";
+modConcat("./index.js", outputFile, function(err, stats) {
+    if(err) throw err;
+    console.log(stats.files.length + " were combined into " + outputFile);
+});
