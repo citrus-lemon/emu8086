eb38a23321118bef7ef87647d1bc2b2b33fa6d1c
WindProphet <lihanyuan1996@gmail.com>
2017-04-02 18:19:12 +0800

    memory page optimize

--- src/Component.js
+++ src/Component.js
@@ -59,9 +59,8 @@ class Register extends Component {
 class Memory extends Component {
   constructor() {
     super()
-    this.state = {start: 0}
+    this.state = {point: 580}
     this.lineheight = 16
-    this.position = 0
     top.hello = this
   }
 
@@ -69,85 +68,161 @@ class Memory extends Component {
     let line = 170
     let minus = 70
     let style = this.props.frame
-    if (this.state.start < 0) {
-      this.state.start = 0
+    if (this.state.point < 0) {
+      this.state.point = 0
     }
-    // console.log("memory: ",this.state.start)
     return (
       <div className="memory-space" style={style}>
         <div className="model-tag">Memory</div>
         <div className="model-frame">
-          <div style={{top: '-20px', left: '-20px', right: '-20px', position: 'absolute', height: 35, boxShadow: '0 2px 4px 0 rgba(0,0,0,0.50)', zIndex: 1}}></div>
+          <div style={{top: '-20px', left: '-20px', right: '-20px', position: 'absolute', height: 35, boxShadow: '0 2px 4px 0 rgba(0,0,0,0.50)', zIndex: 1}} onClick={() => {
+            this.searchinput && this.searchinput.focus()
+            }}></div>
           <div style={{bottom: '-20px', left: '-20px', right: '-20px', position: 'absolute', height: 30, boxShadow: '0 -2px 4px 0 rgba(0,0,0,0.50)', zIndex: 1}}></div>
           <div style={{height: '20px', position: 'absolute', top: 0, left: '10px', right: '10px'}}>
             <div className="memory-label">
               <input type="text" style={{
-                'color': '#888',
-                'border': 'none',
-                'width': '100%',
-                'height': '100%',
-                'background-color': 'rgba(0,0,0,0)',
-                'text-align': 'center'
-              }}/>
+                color: '#888',
+                border: 'none',
+                width: '100%',
+                height: '100%',
+                backgroundColor: 'rgba(0,0,0,0)',
+                textAlign: 'center',
+                top: '-6px',
+                left: 0,
+                position: 'absolute'
+              }} onFocus={(e) => {
+                this.searchinput.value = ''
+              }} onBlur={(e) => {
+                let num;
+                try {
+                  num = parseInt(this.searchinput.value, 16)
+                } catch (error) {
+
+                }
+                this.searchinput.value = '';
+                (num && !isNaN(num)) && this.setState({point: num});
+              }} ref={(el) => {this.searchinput = el}}/>
             </div>
             <div className="memory-table" style={{'color': '#888'}}>
               {[...Array(16).keys()].map((e) => (<div className="memory-block-element">{padz(e,2)}</div>))}
             </div>
           </div>
           <div style={{overflow: 'hidden', position: 'absolute', top: 20, left: 10, right: 10, bottom: 10}} ref={(el) => {
+            const line = (pos) => {
+              let element = document.createElement("div")
+              element.className = "memory-line"
+              let label = document.createElement("div")
+              label.className = "memory-label"
+              let table = document.createElement("div")
+              table.className = "memory-table"
+              element.appendChild(label)
+              element.appendChild(table)
+              element.setAttribute("style",`top: ${pos*16}px`)
+              label.innerText = padz(pos*16,4);
+              element.setAttribute("data-postion",label.innerText);
+              [...Array(16).keys()].forEach((e) => {
+                let byte = document.createElement("div")
+                byte.className = "memory-block-element"
+                byte.innerText = padz(this.props.cpu.memory(pos*16+e,0),2)
+                table.appendChild(byte)
+              })
+              return element
+            }
+            const line30 = (pos30) => {
+              let element = document.createElement("div");
+              element.setAttribute("data-range",`${pos30*30}-${pos30*30+29}`);
+              [...Array(30).keys()].forEach((e) => {
+                element.appendChild(line(pos30*30 + e))
+              })
+              return element
+            }
+            let pos30 = parseInt(this.state.point/16/30)
+            const height = 16
+            let page = document.createElement("div");
+            let slide = height*(parseInt(this.state.point / 16) - 4);
+            (slide < 0) && (slide = 0);
+            (pos30 < 0) && (pos30 = 0);
+            let movestart
+            console.log(movestart)
+            const translate = (s) => {
+              page.style.transform = `translate3d(0px, -${s}px, 0px)`
+            }
+            translate(slide)
+            let up,mid,down
+            const first_render = (pos) => {
+              let _p30 = parseInt(pos / 30)
+              pos30 = _p30
+              up = (_p30 - 1 >= 0) ? line30(_p30 - 1) : void(0)
+              mid = (_p30 >= 0) ? line30(_p30) : void(0)
+              down = (_p30 + 1 >= 0) ? line30(_p30 + 1) : void(0)
+              up && page.appendChild(up)
+              mid && page.appendChild(mid)
+              down && page.appendChild(down)
+            }
+            first_render(parseInt(this.state.point / 16))
+            el.appendChild(page)
+            const render = (pos) => {
+              let _p30 = pos ? parseInt(pos / height / 30) : parseInt(slide / height / 30)
+              if (_p30 == pos30) {
+
+              }
+              else if (_p30 == pos30 + 1) {
+                up && page.removeChild(up)
+                up = mid
+                mid = down
+                down = (_p30 + 1 >= 0) ? line30(_p30 + 1) : void(0)
+                down && page.appendChild(down)
+              }
+              else if (_p30 == pos30 - 1) {
+                down && page.removeChild(down)
+                down = mid
+                mid = up
+                up = (_p30 - 1 >= 0) ? line30(_p30 - 1) : void(0)
+                up && page.appendChild(up)
+              }
+              else {
+                up && page.removeChild(up)
+                mid && page.removeChild(mid)
+                down && page.removeChild(down)
+                up = (_p30 - 1 >= 0) ? line30(_p30 - 1) : void(0)
+                mid = (_p30 >= 0) ? line30(_p30) : void(0)
+                down = (_p30 + 1 >= 0) ? line30(_p30 + 1) : void(0)
+                up && page.appendChild(up)
+                mid && page.appendChild(mid)
+                down && page.appendChild(down)
+              }
+              pos30 = _p30
+            }
+
+            //events
+
             if (!this.pageel && el) {
-              el.addEventListener("DOMMouseScroll",(e) => {console.log(e)})
               el.addEventListener("mousewheel",(e) => {
-                {/*console.log(e.deltaY)*/}
-                let t = (this.position + e.deltaY) > 0 ? this.position + e.deltaY : 0
-                this.memoryPageElement.style.transform = `translate3d(0px, -${t}px, 0px)`
-                if (Math.abs(parseInt(t/16) - parseInt(this.state.start)) > 20 || (parseInt(t/16) < 5 && this.state.start != 0)) {
-                  {/*console.log('hello',t/16)*/}
-                  this.setState({start: parseInt(this.position / 16)})
-                }
-                this.position = t
+                console.log(e.deltaY)
+                let t = (slide + e.deltaY) > 0 ? slide + e.deltaY : 0
+                translate(t)
+                slide = t
+                render()
               })
               el.addEventListener("touchmove",(e) => {
-                if (!this.pos || isNaN(this.pos)) {this.pos = e.layerY}
-                let t = (this.position + this.pos - e.layerY) > 0 ? this.position + this.pos - e.layerY : 0
-                this.memoryPageElement.style.transform = `translate3d(0px, -${t}px, 0px)`
+                {/*console.log(e.layerY);*/}
+                if (!movestart || isNaN(movestart)) {movestart = e.layerY}
+                let t = (slide + movestart - e.layerY) > 0 ? slide + movestart - e.layerY : 0
+                translate(t)
+                render(t)
               })
               el.addEventListener("touchend", (e) => {
-                if (!this.pos || isNaN(this.pos)) {this.pos = e.layerY}
-                let t = (this.position + this.pos - e.layerY) > 0 ? this.position + this.pos - e.layerY : 0
-                this.memoryPageElement.style.transform = `translate3d(0px, -${t}px, 0px)`
-                if (Math.abs(parseInt(t/16) - parseInt(this.state.start)) > 20 || (parseInt(t/16) < 5 && this.state.start != 0)) {
-                  console.log("touchend",e.layerY)
-                  this.setState({start: parseInt(this.position / 16)})
-                }
-                this.position += this.pos - e.layerY
-                this.pos = void(0)
+                if (!movestart || isNaN(movestart)) {movestart = e.layerY}
+                let t = (slide + movestart - e.layerY) > 0 ? slide + movestart - e.layerY : 0
+                page.style.transform = `translate3d(0px, -${t}px, 0px)`
+                slide = t
+                render()
+                movestart = void(0)
               })
             }
             this.pageel = el
           }}>
-            <div ref={(el) => {
-                this.memoryPageElement = el
-              }} style={{position: 'absolute', height: '100%', width: '100%'}}>
-              <div className="memory-label" style={{color: '#888'}}>
-                {[...Array(line).keys()].map((r) => (
-                  (r >= minus) ?
-                  <div className="memory-line" style={{
-                    top: (r-minus+this.state.start)*16 + 'px'
-                  }}>{padz((r-minus+this.state.start)*16,4)}</div> : void(0)
-                ))}
-              </div>
-              <div className="memory-table">
-                {[...Array(line).keys()].map((r) => (
-                  (r >= minus) ?
-                  <div className="memory-line" style={{
-                    top: (r-minus+this.state.start)*16 + 'px'
-                  }}>{[...Array(16).keys()].map((e) => (<div className="memory-block-element">
-                    {padz(this.props.cpu.memory((r-minus+this.state.start)*16+e,0),2)}
-                  </div>))}</div> : void(0)
-                ))}
-              </div>
-            </div>
           </div>
         </div>
       </div>
